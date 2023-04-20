714ee47a32a0d35bf769b334facd2839b24c4ce8
Bruno Canini <brunocanini1994@gmail.com>
2023-04-20 19:48:24 +0200

    button

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// --------------------------------------------------------------------------
+
 const arrayNomiImg = [ 
     "./img/01.webp",
     "./img/02.webp",
@@ -21,3 +23,51 @@ for (let c = 0; c < arrayNomiImg.length; c++) {
     carosello.append(imgSlide);
 
 }
+
+// -----------------------------------------------------------------------
+
+const btnAvanti = document.getElementById("avantiJs")
+
+// adesso creo un array di ELEMENTI img
+const arrayElementiImg = document.querySelectorAll(".carosello > img")
+// adesso posso modificarli tramite l indice
+                // esempio
+// arrayElementiImg[0].classList.remove("visible")
+// arrayElementiImg[1].classList.add("visible")
+
+let indiceIniziale = 0
+
+btnAvanti.addEventListener("click" , function(){
+
+    arrayElementiImg[indiceIniziale].classList.remove("visible")
+
+    indiceIniziale++;
+
+    if(indiceIniziale == arrayElementiImg.length){
+        indiceIniziale = 0;
+    }
+
+    arrayElementiImg[indiceIniziale].classList.add("visible")
+
+})
+
+// -----------------------------------
+
+const btnIndietro = document.getElementById("indietroJs")
+
+btnIndietro.addEventListener("click" , function(){
+
+    arrayElementiImg[indiceIniziale].classList.remove("visible")
+
+    indiceIniziale--;
+
+    // if(indiceIniziale == 0){
+    //     indiceIniziale = ;
+    // }
+
+    arrayElementiImg[indiceIniziale].classList.add("visible")
+
+})
+
+
+
